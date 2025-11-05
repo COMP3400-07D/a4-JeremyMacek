8d381c4466ef185569e456976a52de337e598a4f
Jeremy Macek <macekjeremy@kali.os>
2025-11-05 17:53:39 -0500

    Completed Part B

--- partb.c
+++ partb.c
@@ -1,8 +1,39 @@
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
 
 // TODO: Include more header files (read the manpages to find out more!)
 
 int main(int argc, const char* argv[]) {
+
+    if (argc < 2){
+        printf("ERROR: No arguments");
+        return 1;
+    }
+
+    if ((argc - 1) % 2 == 0){
+        int items = argc / 2;
+
+        char * args[] = {"echo", argv[items], argv[items + 1], NULL};
+        int eret = execv("/bin/echo", args);
+
+        if (eret == -1){
+            perror("exec");
+            return 1;
+        } 
+    }
+    else{
+        int items = argc / 2;
+
+        char * args[] = {"echo", argv[items], NULL};
+        int eret = execv("/bin/echo", args);
+
+        if (eret == -1){
+            perror("exec");
+            return 1;
+        }
+    }
+
     return 0;
 }
