d4216121c048b551e04f32d4368a2c986f29b075
Jeremy Macek <macekjeremy@kali.os>
2025-11-05 17:51:43 -0500

    Completed Part A

--- parta.c
+++ parta.c
@@ -1,8 +1,27 @@
 #include <stdbool.h>
 #include <stddef.h>
-
-// TODO: Include more header files (read the manpages to find out more!)
+#include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, const char* argv[]) {
+    
+    if (argc < 2){
+        printf("ERROR: No arguments");
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++){
+        const char* word = argv[i];
+        
+        
+        for (int j = 0; word[j] != '\0'; j++){
+            printf("%c", toupper(word[j]));
+        }
+
+        if (i < argc - 1){
+            printf(",");
+        }
+    }
+    printf("\n");
     return 0;
 }
